f6821ae4661d1d34797c8a6182a3c43330301c25
Zach Starnes <zachstarnes@gmail.com>
2014-05-02 09:55:03 -0400

    removed test code

--- src/Project/Controllers/WelcomeController.php
+++ src/Project/Controllers/WelcomeController.php
@@ -1,9 +1,5 @@
 <?php
 
-/**
- * You must use a namespace based on where the file is located. 
- */
-
 namespace Project\Controllers;
 
 use Umbrella\Source\Controller;
@@ -13,17 +9,6 @@ class WelcomeController extends Controller
 {
     public function indexAction()
     {
-        $user = new User();
-        $em = $this->getManager();
-
-        $user->setFirstName('Zach');
-        $user->setLastName('Starnes');
-        $user->setEmail('zachstarnes@gmail.com');
-        $user->setUsername('test');
-
-        $em->persist($user);
-        $em->flush();
-
         $text = 'Welcome back!';
         
         $this->render('welcome.html.twig', array(
